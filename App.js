37518001e54cd7ec6a14a8653267b888427029f1
Jaisonjohn78 <jaisonjohn78.com@gmail.com>
2023-02-26 01:59:56 +0530

    Login Changes

--- App.js
+++ App.js
@@ -2,7 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 import AmbScreen from './src/mainScreens/AmbScreen';
-import RootNavigator from './src/navFiles/RootNavigator';
+// import RootNavigator from './src/navFiles/RootNavigator';
+// import RootNavigator from './src/mainScreens/Login';
+import RootNavigator from './src/mainScreens/Signup';
+
 
 export default function App() {
   return (
